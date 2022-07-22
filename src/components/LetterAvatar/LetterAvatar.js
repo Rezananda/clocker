0ad1be20ad836403abf583f7411d58cdbb261f04
Made Rezananda Putra <maderezanandaputra@172-10-33-10.lightspeed.bcvloh.sbcglobal.net>
2022-07-22 09:20:38 +0700

    uppercase avatar

--- src/components/LetterAvatar/LetterAvatar.js
+++ src/components/LetterAvatar/LetterAvatar.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const LetterAvatar = ({letter, handleClick, additionalClass}) => {
   return (
-      <div onClick={handleClick} className={`h-10 w-10 text-lg font-bold text-white bg-blue-500 rounded-full flex justify-center items-center ${additionalClass}`}>{letter}</div>
+      <div onClick={handleClick} className={`h-10 w-10 text-lg font-bold text-white bg-blue-500 rounded-full flex justify-center items-center uppercase ${additionalClass}`}>{letter}</div>
   )
 }
 
